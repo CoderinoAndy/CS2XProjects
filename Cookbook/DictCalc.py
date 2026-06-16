7405feca6d5d26dd0a38c780dd44fa2a913825b7
Richard Yu <burritoman@gmail.com>
2026-06-16 15:43:13 -0400

    hello

---
+++ Cookbook/DictCalc.py
@@ -0,0 +1,13 @@
+dicti = {
+    "IBM": 2,
+    "Smarts": 10, 
+    "Stinks": 53.12,
+    "FB": 10.67,
+}
+
+min_price = min(zip(dicti.values(), dicti.keys()))
+max_price = max(zip(dicti.values(), dicti.keys()))
+
+dicti.values(), dicti.keys()))
+for price, name in prices_sorted:
+    print(' ', name, price)
